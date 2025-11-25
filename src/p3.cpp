f967a3e1af7faf72fcf702ad1e52a46c90f09d8f
changyutzu <yutzuchang.mg14@nycu.edu.tw>
2025-11-25 14:05:08 +0800

    Update p3.cpp

--- src/p3.cpp
+++ src/p3.cpp
@@ -2,8 +2,42 @@
 
 #include <iostream>
 using namespace std;
-
+bool isPrime(int n) {
+    if (n <= 1) {
+        return false;
+    }
+    if (n == 2) {
+        return true;
+    }
+        if (n % 2 == 0) {
+        return false;
+    }
+    int limit = static_cast<int>(sqrt(n));
+    for (int i = 3; i <= limit; i += 2) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
 int main() {
+    int n;
+    if (cin >> n) {
+          for (int a = 3; a <= n / 2; a += 2) {
+            int b = n - a;
+            if (isPrime(a)) {
+                if (isPrime(b)) {
+                    cout << a << " " << b << endl;
+                    return 0; 
+                }
+            }
+        }
+        
+        if (n == 4) {
+             cout << "2 2\n";
+             return 0;
+        }
+    }
     
     
     return 0;
