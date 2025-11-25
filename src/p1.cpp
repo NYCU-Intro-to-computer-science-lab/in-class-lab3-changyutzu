b3ece30db86d826f8b32d03871ea6ae54fc6c3da
changyutzu <yutzuchang.mg14@nycu.edu.tw>
2025-11-25 13:37:55 +0800

    Update p1.cpp

--- src/p1.cpp
+++ src/p1.cpp
@@ -2,9 +2,28 @@
 
 #include <iostream>
 using namespace std;
+long long factorial(int n) {
+    if (n == 0) {
+        return 1;
+    }
+    return (long long)n * factorial(n - 1);
+}
 
+long long sum(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    return (long long)n + sum(n - 1);
+}
 int main() {
-    
+    int n;
+    if (cin >> n) {
+        long long factorial_result = factorial(n);
+        cout << factorial_result << "\n";
+        
+        long long sum_result = sum(n);
+        cout << sum_result << endl;
+    }
     
     return 0;
 }
