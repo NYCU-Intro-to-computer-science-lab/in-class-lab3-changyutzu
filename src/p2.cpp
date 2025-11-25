ad50c3cbcd5c9958358375ae51a4e6cb185a89d7
changyutzu <yutzuchang.mg14@nycu.edu.tw>
2025-11-25 13:44:05 +0800

    Update p2.cpp

--- src/p2.cpp
+++ src/p2.cpp
@@ -2,9 +2,17 @@
 
 #include <iostream>
 using namespace std;
-
+int gcd(int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+    return gcd(b, a % b);
+}
 int main() {
-    
+    if (cin >> a >> b) {
+        int result = gcd(a, b);
+        cout << result << endl;
+    }
     
     return 0;
 }
